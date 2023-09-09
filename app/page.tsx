22f09d642772d6c1575e68d8ac32ad2511986d0e
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2023-09-09 23:35:10 +0100

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import { Select, MenuItem, Slider } from '@mui/material'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      
+      hiiiii
+      <Select >
+        <MenuItem value={1}>One</MenuItem>
+      </Select>
+      <Slider />
     </main>
   )
 }
