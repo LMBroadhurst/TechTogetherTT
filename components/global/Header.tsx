0d2b87a7feb58bcae2f80fe981116fb73d6d6bf5
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2023-09-10 23:03:04 +0100

    fin

--- components/global/Header.tsx
+++ components/global/Header.tsx
@@ -5,18 +5,18 @@ type OwnProps = {};
 const Header: FC<OwnProps> = ({}) => {
 
 
-  return <header className='border-b w-full bg-slate-100 shadow-md sticky top-0 z-10'>
+  return <header className='border-b w-full shadow-md sticky top-0 z-10'>
     <section className='flex flex-row justify-between items-center p-4 text-2xl text-slate-800 '>
       <h1 className='font-extrabold'>TechTogether</h1>
 
       {/* Burger Menu */}
-      
-
-          <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
+      <button className='btn btn-ghost'>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8">
+          <path fillRule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clipRule="evenodd" />
         </svg>
-        
+      </button>
 
+    
     </section>
   </header>
 }
