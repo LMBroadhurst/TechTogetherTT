c4d153c6bb770b5df37dd61b6979171f166349c0
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2023-11-05 15:28:26 +0000

    Refactor layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,25 +5,24 @@ import Header from '@/components/global/header/Header'
 import Providers from './Providers'
 import { getServerSession } from 'next-auth'
 import { authOptions } from './api/auth/[...nextauth]/route'
-import { SessionProvider } from 'next-auth/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'TechTogether',
-  description: 'TechTogether',
+    title: 'TechTogether',
+    description: 'TechTogether',
 }
 
-export default async function RootLayout({children}: {children: React.ReactNode}) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
 
-  const session = getServerSession(authOptions)
-  
-  return <html lang="en">
-    <body className={inter.className}>
-      <Providers session={session}>
-        <Header />
-        {children}
-      </Providers>
-    </body>
-  </html>
+    const session = getServerSession(authOptions)
+
+    return <html lang="en">
+        <body className={inter.className}>
+            <Providers session={session}>
+                <Header />
+                {children}
+            </Providers>
+        </body>
+    </html>
 }
