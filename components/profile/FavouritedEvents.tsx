b09dec427c9b8ff1c9ae47092d6dfe494b6e530d
Lewis <lewis1broadhurst@gmail.com>
2023-11-20 22:03:19 +0000

    ...

--- components/profile/FavouritedEvents.tsx
+++ components/profile/FavouritedEvents.tsx
@@ -6,14 +6,14 @@ import { useFilteredUserEventsAndEventsForUser } from './hooks'
 
 export default function FavouritedEvents({ eventData, userEventData }: { eventData: Event[], userEventData: UserEvent[] }) {
 
-    const { events, userEvents } = useFilteredUserEventsAndEventsForUser(eventData, userEventData)
+    // const { events, userEvents } = useFilteredUserEventsAndEventsForUser(eventData, userEventData)
 
     return <section>
         <h2 className='text-lg font-semibold text-slate-500'>Favourited Events</h2>
         <h3 className='text-xl font-medium text-slate-600'>This is your TechTogether profile. Cutomise it to your liking.</h3>
 
         <section className='flex flex-row gap-10'>
-            {
+            {/* {
                 events && events?.length > 0
                     ? events?.map((event: Event) => {
                         return <EventCardSSR
@@ -23,7 +23,7 @@ export default function FavouritedEvents({ eventData, userEventData }: { eventDa
                         />
                     })
                     : null
-            }
+            } */}
         </section>
     </section>
 }
