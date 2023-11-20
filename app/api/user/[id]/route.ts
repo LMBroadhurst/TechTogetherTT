182051b16943dab09e13c4725f9c0b2f52bf9e89
Lewis <lewis1broadhurst@gmail.com>
2023-11-20 22:11:26 +0000

    ...

--- app/api/user/[id]/route.ts
+++ app/api/user/[id]/route.ts
@@ -1,5 +1,4 @@
 import prisma from "@/prisma/db";
-import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
