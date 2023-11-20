ba92870ff6ff94cad82eb03177463d88ef2f4d3a
Lewis <lewis1broadhurst@gmail.com>
2023-11-20 21:33:15 +0000

    ...

--- components/auth/signup/SignupFormView.tsx
+++ components/auth/signup/SignupFormView.tsx
@@ -1,12 +1,12 @@
-import React, {FC, useState} from 'react'
-import {defaultSignupForm} from './defaultSignupFormValues'
-import {Person} from '@/types/person'
+import React, { FC, useState } from 'react'
+import { defaultSignupForm } from './defaultSignupFormValues'
 import SignupFormHeader from './SignupFormHeader'
 // import { usePostPersonMutation } from '@/rtk/person/personAPI'
-import { VContainer } from '../global/Containers'
-import TextInput from '../global/TextInput'
+import { VContainer } from '../../global/Containers'
+import TextInput from '../../global/TextInput'
 
-const SignupFormView: FC = ({}) => {
+
+const SignupFormView: FC = ({ }) => {
 
     // SignupFrom API Code
     // const [signupTrigger, {data, isLoading, isSuccess, isError}] = usePostPersonMutation()
@@ -19,63 +19,59 @@ const SignupFormView: FC = ({}) => {
     }
 
     // Form Related Code
-    const [signupDetails, setSignupDetails] = useState<Person>(defaultSignupForm)
+    const [signupDetails, setSignupDetails] = useState(defaultSignupForm)
     const [isPassword, setIsPassword] = useState<boolean>(true)
     const handlePasswordToggleClick = () => setIsPassword(!isPassword)
 
-    const {firstName, lastName, email, password} = signupDetails
+    const { firstName, lastName, email, password } = signupDetails
 
     const handleSignupFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const key = event.target.name
         const value = event.target.value
-        setSignupDetails({...signupDetails, [key]: value})
+        setSignupDetails({ ...signupDetails, [key]: value })
     }
 
-    
 
-  return (
-    <section className='flex flex-col gap-4 max-w-lg my-1 m-auto'>
-        <SignupFormHeader />
 
-        <VContainer className='gap-4'>
-            <form className='flex flex-col gap-2 items-stretch' onSubmit={handleClickSignup}>
+    return (
+        <section className='flex flex-col gap-4 max-w-lg my-1 m-auto'>
+            <SignupFormHeader />
 
-                <TextInput
-                    id="signupFirstName" label='First Name' inputType='text' value={firstName} 
-                    apiProperty='firstName'
-                    onChange={handleSignupFormChange}
-                />
+            <VContainer className='gap-4'>
+                <form className='flex flex-col gap-2 items-stretch' onSubmit={handleClickSignup}>
 
-                <TextInput 
-                    id="signupLastName" label='Last Name' inputType='text' value={lastName} 
-                    apiProperty='lastName'
-                    onChange={handleSignupFormChange}
-                />
+                    <TextInput
+                        id="signupFirstName" label='First Name' type='text' value={firstName}
+                        onChange={handleSignupFormChange}
+                    />
 
-                <TextInput 
-                    id="signupEmail" label='Email' inputType='text' value={email} 
-                    apiProperty='email'
-                    onChange={handleSignupFormChange}
-                />
+                    <TextInput
+                        id="signupLastName" label='Last Name' type='text' value={lastName}
+                        onChange={handleSignupFormChange}
+                    />
 
-                <TextInput 
-                    id="signupPassword" label='Password' inputType={isPassword ? 'password' : 'text'} value={password}
-                    isPasswordInput 
-                    apiProperty='password'
-                    passwordStatus={isPassword}
-                    onChange={handleSignupFormChange}
-                    onPasswordTextToggle={handlePasswordToggleClick}
-                />
+                    <TextInput
+                        id="signupEmail" label='Email' type='text' value={email}
+                        onChange={handleSignupFormChange}
+                    />
 
-                <button type="submit" className='btn' onClick={handleClickSignup}>
-                    {/* {!isError ? (isLoading ? "..." :  */}
-                    Join TechTogether
-                    {/* ) : "Something went wrong :("} */}
-                </button>
-            </form>
-        </VContainer>
-    </section>
-  )
+                    <TextInput
+                        id="signupPassword" label='Password' type={isPassword ? 'password' : 'text'} value={password}
+                        isPasswordInput
+                        passwordStatus={isPassword}
+                        onChange={handleSignupFormChange}
+                        onPasswordTextToggle={handlePasswordToggleClick}
+                    />
+
+                    <button type="submit" className='btn' onClick={handleClickSignup}>
+                        {/* {!isError ? (isLoading ? "..." :  */}
+                        Join TechTogether
+                        {/* ) : "Something went wrong :("} */}
+                    </button>
+                </form>
+            </VContainer>
+        </section>
+    )
 }
 
 export default SignupFormView;
