7275d87529a2e4c093a552452730f1a7564b68ac
Lewis <lewis1broadhurst@gmail.com>
2023-11-28 19:07:40 +0000

    ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,27 @@
 import './globals.css'
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Header from '@/components/global/header/Header'
 import Providers from './Providers'
 import { getServerSession } from 'next-auth'
 import { authOptions } from './api/auth/[...nextauth]/route'
-import { SessionProvider } from 'next-auth/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'TechTogether',
-  description: 'TechTogether',
-}
+// export const metadata: Metadata = {
+//   title: 'TechTogether',
+//   description: 'TechTogether',
+// }
 
-export default async function RootLayout({children}: {children: React.ReactNode}) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
 
-  const session = getServerSession(authOptions)
-  
-  return <html lang="en">
-    <body className={inter.className}>
-      <Providers session={session}>
-        <Header />
-        {children}
-      </Providers>
-    </body>
-  </html>
+    const session = getServerSession(authOptions)
+
+    return <html lang="en">
+        <body className={inter.className}>
+            <Providers session={session}>
+                <Header />
+                {children}
+            </Providers>
+        </body>
+    </html>
 }
