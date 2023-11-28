ad418ee592db35bf716cb98e6f900b38c5a67598
Lewis <lewis1broadhurst@gmail.com>
2023-11-28 19:51:30 +0000

    ...

--- components/home/HomeEventsContainer.tsx
+++ components/home/HomeEventsContainer.tsx
@@ -3,11 +3,21 @@ import EventCardCSR from "./EventCard"
 
 export default async function HomeEventsContainer() {
 
-    const eventResponse = await fetch(process.env.URL + "/api/event")
+    let eventResponse;
+    let userEventResponse;
+
+    try {
+
+        eventResponse = await fetch(process.env.URL + "/api/event")
+        userEventResponse = await fetch(process.env.URL + "/api/userEvent")
+
+    } catch (error) {
+        return null;
+    }
+
     const resolvedData = await eventResponse.json()
     const events = resolvedData.events as Event[]
 
-    const userEventResponse = await fetch(process.env.URL + "/api/userEvent")
     const resolvedUserEvents = await userEventResponse.json()
     const userEvents = resolvedUserEvents.userEvents as UserEvent[]
 
