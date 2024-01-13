14d7539cf7dc1aa69d8dda0531ea8067f1fa8d4c
Lewis <lewis1broadhurst@gmail.com>
2024-01-13 13:02:14 +0000

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { VContainer } from '@/components/global/Containers'
 import HomeTitle from '@/components/home/HomeTitle'
 import React from 'react'
@@ -6,10 +7,10 @@ import HomeEventsContainer from '@/components/home/HomeEventsContainer'
 
 export default function Home() {
 
-    return <main className='flex flex-col px-5 py-10 gap-16 md:w-[1000px] mx-auto overflow-hidden'>
+    return <main className='flex flex-col px-5 py-10 md:w-[1000px] mx-auto overflow-hidden'>
         <HomeTitle />
 
-        <VContainer className='gap-4 justify-center items-center'>
+        <VContainer className='gap-4 justify-center'>
             <h2 className='text-2xl font-bold'>Recently Created Events</h2>
 
             <HomeEventsContainer />
--- components/home/HomeTitle.tsx
+++ components/home/HomeTitle.tsx
@@ -7,10 +7,10 @@ const HomeTitle = () => {
     // Authentication
     const { data: session } = useSession()
 
-    return <section className='flex flex-col gap-10 py-32'>
+    return <section className='flex flex-col gap-6 py-32'>
         <section>
-            <h2 className='text-lg font-semibold text-slate-500'>{`${session?.user?.name ? session?.user?.name : 'Hey Stranger'}`},</h2>
-            <h3 className='text-2xl font-bold text-slate-600'>Let&apos;s connect you with the TechTogether community</h3>
+            <h2 className='text-2xl font-semibold text-slate-500'>{`${session?.user?.name ? session?.user?.name : 'Hey Stranger'}`},</h2>
+            <h3 className='text-5xl font-bold text-slate-600'>Let&apos;s connect you with the TechTogether community</h3>
         </section>
 
         <p className=''>
@@ -18,7 +18,12 @@ const HomeTitle = () => {
             We provide a space to organise those events that will <span className='text-blue-900 font-bold'>help you to find Hackathon teams, mentors, fellow enthusiasts, and more.</span>
         </p>
 
-        <button className='rounded bg-slate-700 text-white p-2 self-start'>Find an Event</button>
+        <a
+            href="/events"
+            className='rounded bg-slate-700 text-white p-2 self-start hover:bg-slate-600 hover:cursor-pointer'
+        >
+            Find an Event
+        </a>
     </section>
 }
 
